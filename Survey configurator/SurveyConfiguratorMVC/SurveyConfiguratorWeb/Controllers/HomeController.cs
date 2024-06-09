4ebc97af87ddcb11d8492d8383f649f8b6d267df
Hassan Abu Ghreeb <Hassan@SEDCO.CO>
2024-06-09 21:57:53 +0300

    adding a question is working, working on adding all types of questions

--- Survey configurator/SurveyConfiguratorMVC/SurveyConfiguratorWeb/Controllers/HomeController.cs
+++ Survey configurator/SurveyConfiguratorMVC/SurveyConfiguratorWeb/Controllers/HomeController.cs
@@ -13,6 +13,9 @@ namespace SurveyConfiguratorWeb.Controllers
     public class HomeController : Controller
     {
 
+        //constants
+        private const string cQuestionsView = "Questions";
+
         public HomeController()
         {
             //Initialize the questions List and do the 
@@ -34,6 +37,43 @@ namespace SurveyConfiguratorWeb.Controllers
             }
             //handle case of failure to obtain questions
             return View(new List<Question>());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return RedirectToAction(cQuestionsView);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Question pQuestionData, FormCollection data)
+        {
+            try
+            {
+                StarsQuestion hello = new StarsQuestion(pQuestionData, Convert.ToInt32(data["NumberOfStars"]));
+                OperationResult tIsQuestionAdded = QuestionOperations.AddQuestion(hello);
+                if (tIsQuestionAdded.IsSuccess)
+                {
+                    //on a successful question creation
+                    return RedirectToAction(cQuestionsView);
+                }
+                //show error notfication or error page
+                return RedirectToAction(cQuestionsView);
+            }
+            catch(Exception ex)
+            {
+                //log err
+                return RedirectToAction(cQuestionsView);
+            }
         }
 
         [HttpGet]
@@ -67,12 +107,12 @@ namespace SurveyConfiguratorWeb.Controllers
                 OperationResult tAreQuestionsDeleted = QuestionOperations.DeleteQuestion(tQuestionsIds);
                 if (tAreQuestionsDeleted.IsSuccess)
                 {
-                    return RedirectToAction("Questions");
+                    return RedirectToAction(cQuestionsView);
                 }
                 else
                 {
                     //show error in deletion
-                    return RedirectToAction("Questions");
+                    return RedirectToAction(cQuestionsView);
                 }
             }
             catch (Exception ex)
@@ -80,6 +120,12 @@ namespace SurveyConfiguratorWeb.Controllers
                 //log error and show error page
                 return RedirectToAction("Index");
             }
+        }
+
+        public ActionResult GetQuetsionTypeOptions()
+        {
+            //return view based on switch decision
+            return PartialView("_StarsQuestionOptions");
         }
     }
 }
---
+++ Survey configurator/SurveyConfiguratorMVC/SurveyConfiguratorWeb/Models/StarsQuestionOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SurveyConfiguratorWeb.Models
+{
+    public class StarsQuestionOptions
+    {
+        [Required(ErrorMessage ="The number of stars is required")]
+        [Range(3,10,ErrorMessage ="The number of stars must be between 3 and 10")]
+        [DisplayName("Number of stars")]
+        public int NumberOfStars { get; set; }
+    }
+}
