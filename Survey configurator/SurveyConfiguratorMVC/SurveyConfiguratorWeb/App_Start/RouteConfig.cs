c0ae460f4c4854a615a12f60c124931308d3ca62
Hassan Abu Ghreeb <Hassan@SEDCO.CO>
2024-06-25 01:56:16 +0300

    handled the 404 not found error

--- Survey configurator/SurveyConfiguratorMVC/SurveyConfiguratorWeb/App_Start/RouteConfig.cs
+++ Survey configurator/SurveyConfiguratorMVC/SurveyConfiguratorWeb/App_Start/RouteConfig.cs
@@ -12,16 +12,25 @@ namespace SurveyConfiguratorWeb
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //default route for the existing controllers and their actions
             routes.MapRoute(
                 name: "Default",
                 url: "{Controller}/{action}/{id}",
                 defaults: new { controller = "Questions", action = "Index", id = UrlParameter.Optional }
             );
 
+            //route for handling errors
             routes.MapRoute(
                 name: "Errors",
                 url: "Error/{action}/{ErrorMessage}",
-                defaults: new { controller = "Error", action = "ErrorPage", ErrorMessage = "Page not found", id = UrlParameter.Optional }
+                defaults: new { controller = "Error", action = "ErrorPage", ErrorMessage = "Page not found"}
+            );
+
+            // Catch-all route for 404 errors
+            routes.MapRoute(
+                name: "NotFound",
+                url: "{*any}",
+                defaults: new { controller = "Error", action = "ErrorPage", errorMessage = "404 Page not found" }
             );
         }
     }
