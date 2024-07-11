b3f2380e0391688a761ed29c7374cf2ab4b22689
Hassan Abu Ghreeb <Hassan@SEDCO.CO>
2024-07-11 09:49:05 +0300

    changes to data base scripts

--- Authentication database scripts.sql
+++ Authentication database scripts.sql
@@ -1,2 +1,18 @@
-use Authentication_DB;
-select * from RefreshTokens;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Authentication_DB')
+BEGIN
+    CREATE DATABASE Authentication_DB
+    ALTER DATABASE Authentication_DB SET COMPATIBILITY_LEVEL = 160
+END
+GO
+
+USE Authentication_DB
+GO
+
+IF OBJECT_ID('dbo.RefreshTokens', 'U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[RefreshTokens](
+        [Id] [int] NOT NULL PRIMARY KEY,
+        [ExpireDate] [DateTime] NOT NULL
+    )
+END
+GO
